fd4a0a79d0797bf7810952c8e21afa74f39d0c3e
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-04 19:44:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,8 @@
 def join_nested_strings(src)
   results_array = []
   count = 0
+  index = 0
+  element = 0
   while count < src.length do
     if src[index][element].class == String
     results_array << src[index][element]
@@ -9,4 +11,4 @@ def join_nested_strings(src)
   results_array
 end
 
-end
+
