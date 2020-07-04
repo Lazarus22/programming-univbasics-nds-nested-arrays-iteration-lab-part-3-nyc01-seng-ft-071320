cfaf0abe9829f477be49b1ff00bffc5f63284e00
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-04 19:47:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,8 +2,8 @@ def join_nested_strings(src, index, element)
   results_array = []
   count = 0
   while count < src.length do
-    if src[][].class == String
-    results_array << src[][]
+    if src[index][element].class == String
+    results_array << src[index][element]
     count += 1
   end
   results_array
